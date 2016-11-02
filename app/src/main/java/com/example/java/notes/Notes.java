83586d90a3537db1db6f579d315fd62e051fb404
sanekdp <sanekdp@gmail.com>
2016-11-02 17:37:56 +0200

    new

--- app/src/main/java/com/example/java/notes/Notes.java
+++ app/src/main/java/com/example/java/notes/Notes.java
@@ -16,7 +16,7 @@ public class Notes extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        //test Push
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
         recyclerView = (RecyclerView) findViewById(R.id.recycler_notes);
