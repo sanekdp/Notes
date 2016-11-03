e8068550df95135eb87f3f2490059f6e1a8d1c24
sanekdp <sanekdp@gmail.com>
2016-11-03 13:32:38 +0200

    03112016

--- app/src/main/java/com/example/java/notes/Notes.java
+++ app/src/main/java/com/example/java/notes/Notes.java
@@ -1,9 +1,16 @@
 package com.example.java.notes;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.example.java.notes.adapters.NotesAdapter;
 
@@ -13,12 +20,24 @@ import java.util.List;
 public class Notes extends AppCompatActivity {
 
     private RecyclerView recyclerView = null;
+    private Toolbar toolbar = null;
+    private FloatingActionButton fab = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //test Push
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle(R.string.app_name);
+        fab = (FloatingActionButton) findViewById(R.id.floating_btn);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(recyclerView, "click FloatActionBar", Snackbar.LENGTH_SHORT).show();
+            }
+        });
         recyclerView = (RecyclerView) findViewById(R.id.recycler_notes);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
                 recyclerView.setLayoutManager(layoutManager);
@@ -30,5 +49,31 @@ public class Notes extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
         adapter.setDataSource(dataSource);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.notes_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_settings:
+                Snackbar.make(recyclerView, R.string.settings, Snackbar.LENGTH_LONG).show();
+                return true;
+            case R.id.action_help:
+                Snackbar.make(
+                        recyclerView,
+                        R.string.help,
+                        Snackbar.LENGTH_LONG)
+                        .show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
